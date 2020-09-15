cca55dfb007e7800a4b013a7d4a351cbb20c077d
vishalmgada <vishalmgada@gmail.com>
2020-09-15 09:17:57 +0530

    Added code for API configuration

---
+++ src/main/java/api/utilities/ResponseValidation.java
@@ -0,0 +1,64 @@
+package api.utilities;
+
+import io.restassured.response.Response;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import java.lang.reflect.Type;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+@AllArgsConstructor
+public class ResponseValidation {
+    private final Response response;
+
+    /**
+     * This method is used to validate status code of the Response
+     *
+     * @param statusCode - Expected Status Code
+     */
+    public void validateStatusCode(int statusCode) {
+        response.then().assertThat().statusCode(statusCode);
+    }
+
+    /**
+     * This method is used to validate the schema of the response.
+     *
+     * @param schemaFileName - Expected Schema File Name
+     */
+    public void validateJSONSchema(String schemaFileName) {
+        response.then().assertThat().body(matchesJsonSchemaInClasspath(schemaFileName));
+    }
+
+    /**
+     * This method is used to validate the headers of the response.
+     *
+     * @param headers - Expected Headers
+     */
+//    public void validateHeaders(Map<String, String> headers) {
+//        headers.forEach((key, value) -> Assert.assertEquals(value, response.getHeader(key)));
+//
+//    }
+//
+//    /**
+//     * This method is used to validate cookies of the response
+//     *
+//     * @param cookies - Expected Cookies
+//     */
+//    public void validateCookies(Map<String, String> cookies) {
+//        cookies.forEach((key, value) -> Assert.assertEquals(value, response.getCookie(key)));
+//
+//    }
+
+    /**
+     * This method is used to Deserialize the JSON Response into Java Object
+     *
+     * @param Deserializer - Generic Java Object of the Response
+     * @return - Return Deserialized Objects
+     */
+    @SneakyThrows
+    public <T> T deserializeResponse(Class<T> Deserializer) {
+        T deserializer = null;
+            deserializer = Deserializer.getDeclaredConstructor().newInstance();
+
+        return response.as((Type) deserializer.getClass());
+    }
+}
---
+++ src/main/java/common/APIConfiguration.java
@@ -0,0 +1,19 @@
+package common;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class APIConfiguration {
+
+    @JsonProperty(value="baseuri")
+    private String baseuri;
+
+    @JsonProperty(value="resource")
+    private String resource;
+
+    @JsonProperty(value="key")
+    private String key;
+}
